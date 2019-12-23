02aff1e3be2362314f68ac0e81260b34468b329c
Katie Steinmeyer <ksteinmeyer@wustl.edu>
2019-12-22 22:13:27 -0600

    changing colors correctly

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-for(var i = 0; i < 12; i++){
-  document.getElementById('box'+i).addEventListener('click', changeColor);
+var colorChoices = ['pink', 'red', 'blue', 'green', 'yellow', 'orange'];
+var colorMap = new Map();
+for(var i = 0; i < colorChoices.length; i++){ //initalizing the values of the map to 0, because none of the colors have been used yet
+  colorMap.set(colorChoices[i], '0');
+  console.log(colorMap.get(colorChoices[i]));
 }
 
-function changeColor(event){
-  document.getElementById(event.target.id).style.backgroundColor = 'red';
+
+for (var i = 0; i < 12; i++) { //assigning a color value to each block
+  var index = Math.trunc(Math.random() * 6);
+  while(colorMap.get(colorChoices[index]) > 2){ //does not allow a color value to be assigned to more than 2 blocks
+    index = Math.trunc(Math.random() * 6);
+  }
+  colorMap.set(colorChoices[index], colorMap.get(colorChoices[index])+1);
+  document.getElementById('box' + i).setAttribute('data-myColor', colorChoices[index]);
+  document.getElementById('box' + i).addEventListener('click', changeColor);
+}
+
+function changeColor(event) { //changes block to its internal color value if clicked on
+  console.log(event.target.id);
+  console.log(document.getElementById(event.target.id).getAttribute('data-myColor'));
+  document.getElementById(event.target.id).style.backgroundColor = document.getElementById(event.target.id).getAttribute('data-myColor');
 }
