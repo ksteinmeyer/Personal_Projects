22f73db6264fbac6dbb20434a1186f1b48c3b2e4
Katie Steinmeyer <ksteinmeyer@wustl.edu>
2019-12-23 00:45:35 -0600

    pretty good draft, slight glitched when clicking on other gameboard option

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ document.getElementById('button12').addEventListener('click', createBoard);
 document.getElementById('button24').addEventListener('click', createBoard);
 document.getElementById('button36').addEventListener('click', createBoard);
 var numItems;
+var score = 0;
 
-function createBoard(event){
+function createBoard(event) {
   numItems = event.target.id.replace("button", '');
   console.log(numItems);
   for (var i = 0; i < numItems; i++) { //assigning a color value to each block
-    console.log(i);
-    var index = Math.trunc(Math.random() * (numItems/2));
+    var index = Math.trunc(Math.random() * (numItems / 2));
     while (colorMap.get(colorChoices[index]) > 2) { //does not allow a color value to be assigned to more than 2 blocks
-      index = Math.trunc(Math.random() * (numItems/2));
+      index = Math.trunc(Math.random() * (numItems / 2));
     }
     colorMap.set(colorChoices[index], colorMap.get(colorChoices[index]) + 1);
+    document.getElementById("score").innerHTML = score;
+    // document.getElementById('score').appendChild(score);
     var box = document.createElement('article');
-    var button = document.createElement('button');
     box.classList.add('matchingBox');
     box.setAttribute("id", "box" + i);
-    console.log(box.id);
     box.setAttribute('style', 'background-color: black;');
-    console.log(box.style.backgroundColor);
     box.setAttribute('data-myColor', colorChoices[index]);
     box.setAttribute('data-matched', 'false');
+
     document.getElementById('content').appendChild(box);
     box.addEventListener('click', changeColor);
   }
@@ -29,18 +29,16 @@ function createBoard(event){
 
 var clicked = 0;
 var colorChoices = ['pink', 'red', 'blue', 'green', 'yellow', 'orange',
-'magenta', 'maroon', 'DarkBlue', 'aqua', 'brown', 'grey', 'gold', 
-'cyan', 'coral', 'light pink', 'navy', 'purple'];
+  'magenta', 'maroon', 'DarkBlue', 'aqua', 'brown', 'grey', 'gold',
+  'cyan', 'coral', 'light pink', 'navy', 'purple'
+];
 var colorMap = new Map();
 for (var i = 0; i < colorChoices.length; i++) { //initalizing the values of the map to 0, because none of the colors have been used yet
   colorMap.set(colorChoices[i], '0');
-  console.log(colorMap.get(colorChoices[i]));
 }
 
 
 function changeColor(event) { //changes block to its internal color value if clicked on
-  console.log(event.target.id);
-  console.log(document.getElementById(event.target.id).getAttribute('data-myColor'));
   document.getElementById(event.target.id).style.backgroundColor = document.getElementById(event.target.id).getAttribute('data-myColor');
   clicked += 1;
   if (clicked == 2) {
@@ -65,8 +63,14 @@ function isMatch() { //checks to see if two blocks are a match
     document.getElementById(comparing[0]).setAttribute('style', 'background-color: black');
     document.getElementById(comparing[1]).setAttribute('style', 'background-color: black');
   } else {
+    score += 1;
+    console.log(score);
+    document.getElementById("score").innerHTML = score;
     document.getElementById(comparing[0]).setAttribute('data-matched', 'true');
     document.getElementById(comparing[1]).setAttribute('data-matched', 'true');
+    if (score === numItems / 2) {
+      alert('you win!');
+    }
   }
 
 }
